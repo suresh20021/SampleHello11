52a242dfa8cb881df0e81ce5f9757db42948430b
LEGION <LEGION@DESKTOP-VKAQ8VN>
2026-04-01 08:50:30 +0530

    This is my first commit in SampleHello11

---
+++ SampleHello11/src/main/java/com/durgasoft/SampleHello11Application.java
@@ -0,0 +1,14 @@
+package com.durgasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHello11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHello11Application.class, args);
+		System.out.println(" SampleHello11 ");
+	}
+
+}
---
+++ SampleHello11/src/test/java/com/durgasoft/SampleHello11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleHello11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
